d1f4cb49feefbb298e8c67dee3fcd5ffa4e8bd0a
Platoniux <chizzza@gmail.com>
2018-12-26 10:29:14 +0200

    corrected form validator

--- js/task6.js
+++ js/task6.js
@@ -18,24 +18,76 @@ var form = {
   },
 };
 
-var validator = validator();
-console.log(validator(form));
+var validForm = validation();
+console.log(validForm(form));
 
-function validator(obj) {
-  function validation(obj) {
-    return (validation.validName(obj) && validation.validEmail(obj));
+function validation() {
+  var results = [];
+  function checkResults(obj) {
+    for(key in obj) {
+      if(obj[key].validationRules.required) {
+        results.push(checkOnRequired(obj[key]));
+      }
+      if(obj[key].validationRules.minLength) {
+        results.push(checkOnMinLength(obj[key]));
+      }
+      if(obj[key].validationRules.maxLength) {
+        results.push(checkOnMaxLength(obj[key]));
+      }
+      if(obj[key].validationRules.email) {
+        results.push(checkOnEmail(obj[key]));
+      }
+    }
+    return results.every(checkOnResults);
   }
-  validation.validName = function(obj) {
-    var nameLength = obj.name.value.length;
-    var requireMinLength = obj.name.validationRules.minLength;
-    var requireMaxLength = obj.name.validationRules.maxLength;
-    return (nameLength >= requireMinLength && nameLength <= requireMaxLength); 
-  };
-  validation.validEmail = function(obj) {
-    var email = obj.email.value;
+
+  function checkOnRequired(obj) {
+    var value = obj.value;
+    if ((value.length === 0) || value.split('').some(checkOnSpaces)) {
+      obj.errorMessage = 'ERROR: This is required field';
+      return false;
+    }
+    return true;
+  }
+
+  function checkOnMinLength(obj) {
+    var value = obj.value;
+    var requireMinLength = obj.validationRules.minLength;
+    if (value.length >= requireMinLength) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Min length is ' + requireMinLength;
+    return false;
+  }
+
+  function checkOnMaxLength(obj) {
+    var value = obj.value;
+    var requireMaxLength = obj.validationRules.maxLength;
+    if (value.length <= requireMaxLength) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Max length is ' + requireMaxLength;
+    return false;
+  }
+
+  function checkOnEmail(obj) {
+    var email = obj.value;
     var ruOrUa = email.slice(-3);
     var comOrOrg = email.slice(-4);
-    return (email.indexOf('@') !== -1 && (ruOrUa === '.ru' || ruOrUa === '.ua' || comOrOrg === '.com' || comOrOrg === '.org')); 
-  };
-  return validation;
+    if (email.indexOf('@') !== -1 && (ruOrUa === '.ru' || ruOrUa === '.ua' || comOrOrg === '.com' || comOrOrg === '.org')) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Your email is invalid';
+    return false;
+  }
+
+  function checkOnResults(item) {
+    return item === true;
+  }
+
+  function checkOnSpaces(item) {
+    return item === ' ';
+  }
+
+  return checkResults;
 }
